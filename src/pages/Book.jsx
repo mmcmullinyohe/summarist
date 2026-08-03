2b68cdcbf9da37f7ec8a041436fbb001146e898f
mmcmullinyohe <mmcmullinyohe@gmail.com>
2026-08-03 14:18:52 -0500

    book page and css

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,5 +1,61 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "../styles/book.css";
+
 function Book() {
-  return <h1>Book Page</h1>;
+  const { id } = useParams();
+  const [book, setBook] = useState(null);
+const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+  async function fetchBook() {
+    const response = await fetch(
+      `https://us-central1-summaristt.cloudfunctions.net/getBook?id=${id}`
+    );
+
+    const data = await response.json();
+
+    setBook(data);
+    setLoading(false);
+  }
+
+  fetchBook();
+}, [id]);
+
+if (loading) {
+  return <h1>Loading...</h1>;
+}
+
+return (
+  <main className="book-page">
+    <div className="book-container">
+      <img src={book.imageLink} alt={book.title} />
+
+      <div className="book-info">
+        <h1>{book.title}</h1>
+        <h2>{book.author}</h2>
+        <p>{book.subTitle}</p>
+        <p>⭐ {book.averageRating}</p>
+        <div className="book-actions">
+  <button
+    className="book-action-button"
+    onClick={() => window.location.href = `/player/${book.id}`}
+  >
+    Read
+  </button>
+
+  <button
+    className="book-action-button"
+    onClick={() => window.location.href = `/player/${book.id}`}
+  >
+    Listen
+  </button>
+</div>
+        <p>{book.bookDescription}</p>
+      </div>
+    </div>
+  </main>
+);
 }
 
 export default Book;
